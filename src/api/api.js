bc5aef34ae1483068aa27098327a82936f7b0bc2
bamba <bambandiaye386@gmail.com>
2025-06-12 21:37:44 +0000

    message ajout lien render

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 // Base URL for the JSON Server API
-const BASE_URL = 'http://localhost:3002';
+const BASE_URL = 'https://jsonbackend-vvfh.onrender.com';
 
 // Generic fetch function with error handling
 const fetchAPI = async (url, options = {}) => {
